0a7b99283459ad697ab29265710788d9c68d4cdd
Anton Valintsev <anton.journals@gmail.com>
2016-11-03 12:20:30 +0200

    Add new project

---
+++ Presentation/Nop.Web/Models/Common/LanguageModel.cs
@@ -0,0 +1,13 @@
+﻿using Nop.Web.Framework.Mvc;
+
+namespace Nop.Web.Models.Common
+{
+	public partial class LanguageModel : BaseNopEntityModel
+	{
+		public string Name { get; set; }
+
+		public string UniqueSeoCode { get; set; }
+
+		public string FlagImageFileName { get; set; }
+	}
+}
