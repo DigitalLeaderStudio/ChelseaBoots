6eac5f2252032a2eece80f71e846e0a9816b3de5
serhiikalaida <s.kalaida.dev@gmail.com>
2016-12-16 15:10:29 +0200

    header, newsletter updated, homepage new products added

---
+++ Presentation/Nop.Web/Themes/ChelseaBootsTheme/Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web.Mvc;
+using Nop.Core;
+using Nop.Core.Caching;
+using Nop.Core.Domain.Catalog;
+using Nop.Core.Domain.Customers;
+using Nop.Core.Domain.Localization;
+using Nop.Core.Domain.Media;
+using Nop.Core.Domain.Orders;
+using Nop.Core.Domain.Seo;
+using Nop.Core.Domain.Vendors;
+using Nop.Services.Catalog;
+using Nop.Services.Customers;
+using Nop.Services.Directory;
+using Nop.Services.Events;
+using Nop.Services.Helpers;
+using Nop.Services.Localization;
+using Nop.Services.Logging;
+using Nop.Services.Media;
+using Nop.Services.Messages;
+using Nop.Services.Orders;
+using Nop.Services.Security;
+using Nop.Services.Seo;
+using Nop.Services.Shipping;
+using Nop.Services.Stores;
+using Nop.Services.Tax;
+using Nop.Services.Vendors;
+using Nop.Web.Extensions;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Controllers;
+using Nop.Web.Framework.Security;
+using Nop.Web.Framework.Security.Captcha;
+using Nop.Web.Infrastructure.Cache;
+using Nop.Web.Models.Catalog;
+using Nop.Web.Models.Common;
+using Nop.Web.Models.Media;
+using Nop.Web.Themes.ChelseaBootsTheme.Models;
+
+namespace Nop.Web.Controllers
+{
+	public partial class ProductController : BasePublicController
+	{
+        #region New products for home page
+        [NopHttpsRequirement(SslRequirement.No)]
+        public ActionResult HomepageNewProducts()
+        {
+            if (!_catalogSettings.NewProductsEnabled)
+                return Content("");
+
+            var products = _productService.SearchProducts(
+                storeId: _storeContext.CurrentStore.Id,
+                visibleIndividuallyOnly: false,
+                markedAsNewOnly: true,
+                orderBy: ProductSortingEnum.CreatedOn,
+                pageSize: _catalogSettings.NewProductsNumber);
+
+            var model = new List<ProductOverviewModel>();
+            model.AddRange(PrepareProductOverviewModels(products));
+
+            return View(model);
+        }
+        #endregion
+	}
+}
