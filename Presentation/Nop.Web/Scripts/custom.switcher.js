82860ab190263257d96208da8676ec7b0bf91b0e
serhiikalaida <s.kalaida.dev@gmail.com>
2016-12-19 18:18:28 +0200

    user-tools updated and filter btn added for xs devices

---
+++ Presentation/Nop.Web/Scripts/custom.switcher.js
@@ -0,0 +1,17 @@
+﻿// changing class of a filter button
+$(document).ready(function () {
+	document.getElementById("filterSwitch").addEventListener("click",
+		function () {
+			var filters = document.getElementById("filters");
+			var filterSwitch = document.getElementById("filterSwitch");
+			if (filters.style.display == "none" || filters.style.display == "") {
+				filters.style.display = "block";
+				filterSwitch.style.backgroundColor = "#666666";
+			}
+			else {
+				filters.style.display = "none";
+				filterSwitch.style.backgroundColor = "#c68c53";
+			}
+		}
+	);
+});
